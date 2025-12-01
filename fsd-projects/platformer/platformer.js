a5fae0edddceb63c98335ebb2cf61ccc36de9f61
glitterpoundcake <olivia.lawrence.student@madisonpsb.org>
2025-12-01 15:16:31 +0000

    platformer

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,11 +27,11 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
+   createPlatform(1350, 400, 50, 50, "red");
 
 
 
